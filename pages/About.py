b87ab6711483fd3a26a56a8905a0dae2edde52e6
samriddhist <tripathisamriddhi@gmail.com>
2025-09-14 15:26:15 +0200

    Create About.py

---
+++ pages/About.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+
+st.title("About \n ## Samriddhi")
+
+st.markdown("""
+During the Data Science course, I used a dataset containing information about medicinal drug usage and satisfaction. 
+The dataset contained information about the drug effectiveness, user satisfaction and ease of use. For my own project, 
+I conducted various analyses on this dataset, which included a univariate, bivariate and multivariate analysis.
+A Machine Learning model in that project was used to investigate the influences of ease of use and effectiveness on drug satisfaction.
+It was found that these elements did indeed influence drug satisfaction, but the results were not convincing enough
+to say this is the case for every drug and/or situation; therefore, it was concluded that perhaps more 
+investigation would need to take place.
+""")
+
